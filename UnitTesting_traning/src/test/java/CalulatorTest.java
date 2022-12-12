d4211c4d8f29160e632e15d1d087acd5848ec21b
zhou xuan <zhouxuan@MacBook-Pro-van-zhou.local>
2022-12-12 14:14:02 +0100

    changes

--- UnitTesting_traning/src/test/java/CalulatorTest.java
+++ UnitTesting_traning/src/test/java/CalulatorTest.java
@@ -4,37 +4,37 @@ import org.junit.jupiter.api.Test;
 
 public class CalulatorTest {
     @Test
-    public static void testSum(){
+    public void testSum(){
         double result = Calculator.sum(2.75,2.25);
         Assertions.assertEquals(result,5);
     }
     @Test
-    public static void testMul(){
+    public void testMul(){
         double result = Calculator.multiple(2,8);
         Assertions.assertEquals(result,16);
     }
     @Test
-    public static void testDiv(){
+    public void testDiv(){
         double result = Calculator.divide(4,2);
         Assertions.assertEquals(result,2);
     }
     @Test
-    public static void testSub(){
+    public void testSub(){
         double result = Calculator.subtract(7.5,2.4);
         Assertions.assertEquals(result,5.1);
     }
 
     @Test
-    public static void testDivisionbyZero(){
+    public void testDivisionbyZero(){
         Assertions.assertThrows(RuntimeException.class,()->Calculator.divide(0,4));
     }
 
-    public static void main(String [] args){
+  /*  public static void main(String [] args){
         testSum();
         testMul();
         testDiv();
         testSub();
         testDivisionbyZero();
-    }
+    }*/
 
 }
